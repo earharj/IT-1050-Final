f04103ccaefcba98744aba5dbf538c2882870553
Jackie Earhart <jmricha13@yahoo.com>
2016-12-13 20:55:34 -0500

    Final Exam

---
+++ Final Exam/Final Exam/Elevator.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Final_Exam
+{
+    class Elevator
+    {
+        private double MaxWeight;
+        private Passenger[] Occupants;
+
+        public Elevator(int maxOccupants, double maxWeight)
+        {
+            Occupants = new Passenger[maxOccupants];
+            MaxWeight = maxWeight;
+        }
+
+        public void AddOccupant(Passenger passenger, int index)
+        {
+            Occupants[index] = passenger;
+        }
+
+        double TotalWeight = 0;
+
+        public double GetCurrentWeight()
+        {
+            for (int i = 0; i < Occupants.Length; i++)
+            {
+                TotalWeight += Occupants[i].GetWeight();
+
+            }
+            return TotalWeight;
+        }
+
+        public bool IsOverMaxCapacity()
+        {
+            return GetCurrentWeight() > MaxWeight;
+        }
+        
+        
+
+    }
+}
---
+++ Final Exam/Final Exam/Passenger.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Final_Exam
+{
+    class Passenger
+    {
+        private string Name;
+        private double Weight;
+
+        public Passenger(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+
+        public double GetWeight()
+        {
+            return Weight;
+        }
+    } 
+}
